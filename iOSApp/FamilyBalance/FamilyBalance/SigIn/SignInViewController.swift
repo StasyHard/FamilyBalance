801298db4ac0a526ccb6d5fa8d5e285a234383b2
StasyHard <reyngardt.as@yandex.ru>
2020-05-04 13:47:48 +0300

    Add SignInViewController bindings

--- iOSApp/FamilyBalance/FamilyBalance/SigIn/SignInViewController.swift
+++ iOSApp/FamilyBalance/FamilyBalance/SigIn/SignInViewController.swift
@@ -29,18 +29,51 @@ class SignInViewController: UIViewController {
         
         //-------------------------------------------------------------временно
         navigationController?.navigationBar.isHidden = true
-        self.viewModel = SignInViewModel()
+        viewModel = SignInViewModel()
         
-        self.setUpBindings()
+        setUpBindings()
+        setUpCallbacks()
     }
     
     private func setUpBindings() {
         guard let viewModel = self.viewModel else { return }
+     
+        emailTextField.rx.text.orEmpty
+            .bind(to: viewModel.email)
+            .disposed(by: self.disposeBag)
+        
+        passwordTextField.rx.text.orEmpty
+            .bind(to: viewModel.password)
+            .disposed(by: self.disposeBag)
         
         
-        
+        logInButton.rx.tap
+            .bind { [weak self] in self?.viewModel?.signIn() }
+            .disposed(by: self.disposeBag)
         
     }
     
+    private func setUpCallbacks() {
+        guard let viewModel = self.viewModel else { return }
+        
+        viewModel.isSignInActive
+            .bind { [weak self] in self?.handleIsSignInActive(isActive: $0) }
+            .disposed(by: self.disposeBag)
+        
+        viewModel.isLoading
+            .filter { $0 }
+            .bind { [weak self] _ in self?.showProgressView() }
+            .disposed(by: self.disposeBag)
+    }
+    
+    private func handleIsSignInActive(isActive: Bool) {
+        self.logInButton.isEnabled = isActive
+        self.emailTextField.isEnabled = isActive
+        self.passwordTextField.isEnabled = isActive
+    }
+    
+    private func showProgressView() {
+        print("showProgressView")
+    }
 }
 
--- iOSApp/FamilyBalance/FamilyBalance/SigIn/SignInViewModel.swift
+++ iOSApp/FamilyBalance/FamilyBalance/SigIn/SignInViewModel.swift
@@ -9,13 +9,64 @@
 import Foundation
 import RxSwift
 
+
+enum SingInError {
+    case emailIsEmptyError
+    case passwordIsEmptyError
+}
+
 class SignInViewModel {
     
-    let email = BehaviorSubject(value: "")
-    let password = BehaviorSubject(value: "")
-    let isSignInActive = BehaviorSubject<Bool>(value: false)
+    let email = BehaviorSubject<String>(value: "")
+    let password = BehaviorSubject<String>(value: "")
+    let isSignInActive = BehaviorSubject<Bool>(value: true)
     let isLoading = BehaviorSubject<Bool>(value: false)
+    let error = PublishSubject<SingInError>()
     
     private let disposeBag = DisposeBag()
+    
+    
+    func signIn() {
+        Observable
+            .combineLatest(email, password)
+            .take(1)
+            .do(onNext: { [weak self] (email, password) in
+                if email.isEmpty {
+                    self?.error.onNext(.emailIsEmptyError)
+                }
+                if password.isEmpty {
+                    self?.error.onNext(.passwordIsEmptyError)
+                }
+            })
+            .filter { !$0.isEmpty && !$1.isEmpty }
+//            .flatMapLatest { email, password in
+//                print(email)
+//                print(password)
+//                Observable.of(1)
+//                //service.signgIn(email, password)
+//        }
+       .subscribe {
+
+        }
+        
+
+
+//            .subscribe (
+//                {
+//
+//                    self.isLoading.onNext(false)
+//                    //TODO сохранить токен и перейти на следующий экран
+//            },
+//                {
+//                    self.isLoading.onNext(false)
+//                    self.isSignInActive.onNext(true)
+//                    //TODO обработать ошибку
+//            })
+        self.isLoading.onNext(true)
+        self.isSignInActive.onNext(false)
+        
+        
+        print("signIn")
+    }
     
 }
