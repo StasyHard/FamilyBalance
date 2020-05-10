abbce94c02b7c978fc1ffc6be0b613265c1f7a54
StasyHard <reyngardt.as@yandex.ru>
2020-05-10 19:57:46 +0300

    Add Charts library

--- iOSApp/FamilyBalance/FamilyBalance/Screens/Costs/CostsViewController.swift
+++ iOSApp/FamilyBalance/FamilyBalance/Screens/Costs/CostsViewController.swift
@@ -4,7 +4,7 @@ import UIKit
 class CostsViewController: UIViewController {
     
     //MARK: - IBOutlet
-    @IBOutlet weak var pieChart: UIView!
+    
     
         //MARK: - Private properties
     //private lazy var pieChartVC: PieChartViewController
