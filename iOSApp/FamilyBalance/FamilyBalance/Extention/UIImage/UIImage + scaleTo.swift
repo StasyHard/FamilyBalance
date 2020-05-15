aa0d3919eabe41c59fdb311eeff7dfed4c9f8401
StasyHard <reyngardt.as@yandex.ru>
2020-05-15 18:04:24 +0300

    Add filter icon

---
+++ iOSApp/FamilyBalance/FamilyBalance/Extention/UIImage/UIImage + scaleTo.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage + scaleTo.swift
+//  FamilyBalance
+//
+//  Created by Anastasia Reyngardt on 15.05.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import Foundation
