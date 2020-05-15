567ea890148e632225478d1b49acd6132990bc8d
StasyHard <reyngardt.as@yandex.ru>
2020-05-15 18:05:23 +0300

    Add UIImage + scaleTo extention

--- iOSApp/FamilyBalance/FamilyBalance/Extention/UIImage/UIImage + scaleTo.swift
+++ iOSApp/FamilyBalance/FamilyBalance/Extention/UIImage/UIImage + scaleTo.swift
@@ -1,9 +1,12 @@
-//
-//  UIImage + scaleTo.swift
-//  FamilyBalance
-//
-//  Created by Anastasia Reyngardt on 15.05.2020.
-//  Copyright © 2020 GermanyHome. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    func scaleTo(_ newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage ?? self
+    }
+}
