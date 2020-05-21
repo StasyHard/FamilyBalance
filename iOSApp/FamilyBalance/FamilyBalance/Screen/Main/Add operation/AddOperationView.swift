97683dde8bfb1a8e976bbafca2636bc8f6528038
StasyHard <reyngardt.as@yandex.ru>
2020-05-21 16:04:30 +0300

    Add AddOperationTableView display

--- iOSApp/FamilyBalance/FamilyBalance/Screen/Main/Add operation/AddOperationView.swift
+++ iOSApp/FamilyBalance/FamilyBalance/Screen/Main/Add operation/AddOperationView.swift
@@ -9,17 +9,48 @@ protocol AddOperationViewImplementation: class {
 
 final class AddOperationView: UIView {
     
-    @IBOutlet weak var incomeOrCostsControl: UISegmentedControl!
-    @IBOutlet weak var addOperationTableView: UITableView!
-    @IBAction func saveButtonTapped(_ sender: BlueRoundedButton) {
+    @IBOutlet weak var operationControl: UISegmentedControl!
+    @IBOutlet weak var addOperationTableView: UITableView! {
+        didSet {
+            addOperationTableView.backgroundColor = AppColors.backgroundColor
+            addOperationTableView.delegate = tableViewProvider
+            addOperationTableView.dataSource = tableViewProvider
+            addOperationTableView.tableFooterView = UIView()
+        }
     }
+    
+        //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        backgroundColor = AppColors.backgroundColor
+    }
+
     
     //MARK: - Private properties
     private var actionsDelegate: AddOperationViewActions?
     private let tableViewProvider = AddOperationTableViewProvider()
     
-
-
+    @IBAction func incommeOrCostControlTapped(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            tableViewProvider.operation = .cost
+        }
+        if sender.selectedSegmentIndex == 1 {
+            tableViewProvider.operation = .income
+        }
+        addOperationTableView.reloadData()
+    }
+    @IBAction func saveButtonTapped(_ sender: BlueRoundedButton) {
+    }
+    
 }
 
 
